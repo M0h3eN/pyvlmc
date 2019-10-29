cad0dea49efb5bf86162db49874be9af72163286
mohsen hadianpour <mohsenhadianpour@gmail.com>
2019-10-29 18:49:16 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pyvlmc
+
+it is pyhton3 port Pyvlmc from [Variable Length Markov Chains](https://github.com/magarick/pyvlmc.git)
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import numpy as np
+
+setup(name='pyvlmc',
+      version='0.1',
+      description='Variable length markov chains',
+      author='Mohsen Hadianpour',
+      author_email='mohsenhadianpour@gmail.com',
+      install_requires=['numpy', 'scipy', 'pandas'],
+      include_dirs=[np.get_include(), ],
+      packages=['pyvlmc']
+     )
+
+
+
